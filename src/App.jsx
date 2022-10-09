0bf6aab15c0871e1816e2553447b8757f5e6d8d2
Ankit <ankit.95sharma@gmail.com>
2022-10-09 20:38:24 +0530

    update title and favicon, add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import twitterLogo from "./images/brand-logos/logo-twitter.svg";
+import githubLogo from "./images/brand-logos/logo-github.svg";
+import linkedinLogo from "./images/brand-logos/logo-linkedin.svg";
+import mailLogo from "./images/brand-logos/mail.svg";
+import angelLogo from "./images/brand-logos/angel-icon.svg";
+
 
 const webSeriesDb = {
   comedy: [
@@ -61,7 +67,7 @@ function App() {
               margin: "16px 6px",
               color: "whitesmoke",
               cursor: "pointer",
-              fontSize: "16px"
+              fontSize: "16px",
             }}>
             {genre}
           </button>
@@ -81,7 +87,12 @@ function App() {
                 margin: "auto",
                 marginTop: "10px",
               }}>
-              <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}>
                 <div style={{ fontSize: "larger" }}> {series.name} </div>
                 <div> {series.rating} </div>
               </div>
@@ -89,6 +100,59 @@ function App() {
           ))}
         </ul>
       </div>
+      <footer className="footer">
+        <div className="footer-header">
+          © | 2022 |{" "}
+          <a
+            className="link"
+            href="http://ankit-sharma15.netlify.app"
+            target={"_blank"}>
+            Ankit Sharma
+          </a>{" "}
+        </div>
+        <ul className="list-non-bullet link-social">
+          <li className="list-item-inline">
+            <a
+              className="link"
+              href="https://twitter.com/ankit1595"
+              target="_blank">
+              <img src={twitterLogo} alt="twitter-logo" />
+            </a>
+          </li>
+          <li className="list-item-inline">
+            <a
+              className="link"
+              href="https://github.com/ankit1595"
+              target="_blank">
+              <img src={githubLogo} alt="github-logo" />
+            </a>
+          </li>
+          <li className="list-item-inline">
+            <a
+              className="link"
+              href="https://www.linkedin.com/in/ankit1595"
+              target="_blank">
+              <img src={linkedinLogo} alt="linkedin-logo" />
+            </a>
+          </li>
+          <li className="list-item-inline">
+            <a
+              className="link"
+              href="mailto:ankit.95sharma@gmail.com"
+              target="_blank">
+              <img src={mailLogo} alt="mail-logo" />
+            </a>
+          </li>
+          <li className="list-item-inline">
+            <a
+              className="link"
+              href="https://www.angel.co/u/ankit1595"
+              target="_blank">
+              <img src={angelLogo} alt="angel-logo" />
+            </a>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 }
